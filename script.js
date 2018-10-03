80e416be7015c3081f5973cb2f1c5bfe031f271f
Lukas Babaliauskas <l.babaliauskas@gmail.com>
2018-10-03 17:02:33 -0600

    more exercise

--- script.js
+++ script.js
@@ -199,3 +199,254 @@ var summation = function (num) {
     }
     return sum
   }
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// The museum of incredible dull things
+// The museum of incredible dull things wants to get rid of some exhibitions. Miriam, the interior architect, comes up with a plan to remove the most boring exhibitions. She gives them a rating, and then removes the one with the lowest rating.
+
+// However, just as she finished rating all exhibitions, she's off to an important fair, so she asks you to write a program that tells her the ratings of the items after one removed the lowest one. Fair enough.
+
+// Task
+// Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
+
+// Don't change the order of the elements that are left.
+
+function removeSmallest(numbers) {
+    if (numbers.length < 2) {
+        return []
+    }
+    var num = numbers[0]
+    var index = 0
+    for (var i = 0; i < numbers.length; i++) {
+       if (num > numbers[i]) {
+        num = numbers[i]
+        index = i
+       }
+    }
+    var answer = numbers.filter((el, i) => i !== index)
+    return answer
+}
+
+removeSmallest([10,2,4,6,5,1,3])
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+// The marketing team is spending way too much time typing in hashtags.
+// Let's help them with out own Hashtag Generator!
+
+// Here's the deal:
+
+// It must start with a hashtag (#).
+// All words must have their first letter capitalized.
+// If the final result is longer than 140 chars it must return false.
+// If the input or the result is an empty string it must return false.
+
+function generateHashtag (str) {
+    if (str === '') {
+        return false
+    }
+
+    var lower = str.split(' ');
+    var stringg = []
+    for ( let i = 0; i < lower.length; i++) {
+        var upper = lower[i].split('').map((e,i) => i===0 ? e.toUpperCase() : e).join('')
+        stringg.push(upper)
+    }
+    stringg.unshift('#')
+
+    if (stringg.join('').length > 140) {
+        return false
+    } else {
+        return stringg.join('')
+    }
+  }
+
+generateHashtag('asd fasf asdf')
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// You are given an input string.
+
+// For each symbol in the string if it's the first character occurence, replace it with a '1', else replace it with the amount of times you've already seen it...
+
+// But will your code be performant enough?
+
+function numericals(s){
+    let string = s.split('')
+    let obj = {}
+    var result = []
+
+    for (var i = 0; i < string.length; i++) {
+        if (obj.hasOwnProperty(string[i])) {
+            obj[string[i]] += 1
+            result.push(obj[string[i]])
+        } else {
+            obj[string[i]] = 1
+            result.push(obj[string[i]])
+        }
+    }
+    return result
+}
+numericals('Hello, World')
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// If we were to set up a Tic-Tac-Toe game, we would want to know whether the board's current state is solved, wouldn't we? Our goal is to create a function that will check that for us!
+
+// Assume that the board comes in the form of a 3x3 array, where the value is 0 if a spot is empty, 1 if it is an "X", or 2 if it is an "O", like so:
+
+// [[0, 0, 1],
+//  [0, 1, 2],
+//  [2, 1, 0]]
+// We want our function to return:
+
+// -1 if the board is not yet finished (there are empty spots),
+// 1 if "X" won,
+// 2 if "O" won,
+// 0 if it's a cat's game (i.e. a draw).
+// You may assume that the board passed in is valid in the context of a game of Tic-Tac-Toe.
+
+function isSolved(board) {
+    let empty;
+    for ( let i = 0; i < board.length; i++) {
+        console.log(board[i])
+        for (let m = 0; m < board[i].length; m++) {
+            console.log(board[i][m])
+            if (board[i][m] === 0) {
+                empty = true
+            }
+        }
+    }
+
+    if (board[0][0] === 1 && board[1][1] === 1 && board[2][2] === 1) {
+        return 1
+    } else if (board[0][1] === 1 && board[1][1] === 1 && board[2][1] === 1) {
+        return 1
+    } else if (board[0][2] === 1 && board[1][1] === 1 && board[2][0] === 1) {
+        return 1
+    } else if (board[0][0] === 1 && board[0][1] === 1 && board[0][2] === 1) {
+        return 1
+    } else if (board[1][0] === 1 && board[1][1] === 1 && board[1][2] === 1) {
+        return 1
+    } else if (board[2][0] === 1 && board[2][1] === 1 && board[2][2] === 1) {
+        return 1
+    } else if (board[0][0] === 1 && board[1][0] === 1 && board[2][0] === 1) {
+        return 1
+    } else if (board[0][1] === 1 && board[1][1] === 1 && board[2][1] === 1) {
+        return 1
+    } else if (board[0][2] === 1 && board[1][2] === 1 && board[2][2] === 1) {
+        return 1
+    } else if (board[0][0] === 2 && board[1][1] === 2 && board[2][2] === 2) {
+        return 2
+    } else if (board[0][1] === 2 && board[1][1] === 2 && board[2][1] === 2) {
+        return 2
+    } else if (board[0][2] === 2 && board[1][1] === 2 && board[2][0] === 2) {
+        return 2
+    } else if (board[0][0] === 2 && board[0][1] === 2 && board[0][2] === 2) {
+        return 2
+    } else if (board[1][0] === 2 && board[1][1] === 2 && board[1][2] === 2) {
+        return 2
+    } else if (board[2][0] === 2 && board[2][1] === 2 && board[2][2] === 2) {
+        return 2
+    } else if (board[0][0] === 2 && board[1][0] === 2 && board[2][0] === 2) {
+        return 2
+    } else if (board[0][1] === 2 && board[1][1] === 2 && board[2][1] === 2) {
+        return 2
+    } else if (board[0][2] === 2 && board[1][2] === 2 && board[2][2] === 2) {
+        return 2
+    } else if ( empty === true ) {
+        return -1
+    } else {
+        return 0
+    }
+  }
+
+isSolved([[2,1,1], [2,1,2], [2,2,1]])
+
+///////////////////////////////////////////////////////////////////////
+
+// Write a simple parser that will parse and run Deadfish.
+
+// Deadfish has 4 commands, each 1 character long:
+
+// i increments the value (initially 0)
+// d decrements the value
+// s squares the value
+// o outputs the value into the return array
+// Invalid characters should be ignored.
+
+function parse( data ){
+    let letter = data.split('')
+    let answerArr = []
+    let num = 0
+
+    for (let i = 0; i < letter.length; i++) {
+        if ( letter[i] === 'i') {
+            num += 1
+        } else if ( letter[i] === 'd') {
+            num -= 1
+        } else if ( letter[i] === 's') {
+            num *= num
+        } else if ( letter[i] === 'o') {
+            answerArr.push(num)
+        }
+    }
+    return answerArr
+}
+
+console.log(parse('iiisdoso'))
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Write a function that takes an array of values and moves all elements that are zero to the end of the array, otherwise preserving the order of the array. The zero elements must also maintain the order in which they occurred.
+
+// For example, the following array
+
+// [7, 2, 3, 0, 4, 6, 0, 0, 13, 0, 78, 0, 0, 19, 14]
+
+// is transformed into
+
+// [7, 2, 3, 4, 6, 13, 78, 19, 14, 0, 0, 0, 0, 0, 0]
+
+// Zero elements are defined by either 0 or "0". Some tests may include elements that are not number literals.
+
+// You are NOT allowed to use any temporary arrays or objects. You are also not allowed to use any Array.prototype or Object.prototype methods.
+
+
+function removeZeros(array) {
+    // Sort "array" so that all elements with the value of zero are moved to the
+    // end of the array, while the other elements maintain order.
+    // [0, 1, 2, 0, 3] --> [1, 2, 3, 0, 0]
+    // Zero elements also maintain order in which they occurred.
+    // [0, "0", 1, 2, 3] --> [1, 2, 3, 0, "0"]
+    
+    // Do not use any temporary arrays or objects. Additionally, you're not able
+    // to use any Array or Object prototype methods such as .shift(), .push(), etc
+    
+    // the correctly sorted array should be returned.
+    
+
+    let newArr = []
+    let answer = array.filter( e => {
+        return e !== 0 && e !== '0'
+    })
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === 0 || array[i] === '0') {
+            newArr[newArr.length] = array[i]
+        }
+    }
+    if (newArr.length > 0) {
+        for ( let m = 0; m < newArr.length; m++) {
+            answer[answer.length] = newArr[m]
+        }
+    }
+    return answer
+  }
+
+removeZeros([7, 2, 3, 0, 4, 6, 0, 0, 13, 0, 78, '0', 0, 19, 14])
